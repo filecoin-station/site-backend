1c4d6ffc2534f8125f003a24617b4af92f7c76a8
Julian Gruber <julian@juliangruber.com>
2023-05-30 13:12:47 +0200

    add redis cache for slow `jobs-completed` (#4)
    * add redis cache for slow `jobs-completed`
    
    * ci: update deno
    
    * test

--- lib/getJobsCompletedCount.ts
+++ lib/getJobsCompletedCount.ts
@@ -1,4 +1,27 @@
+import { Redis } from "https://deno.land/x/upstash_redis@v1.20.6/mod.ts";
+
 export default async (): Promise<number> => {
+  const redis = new Redis({
+    url: 'https://us1-above-grub-40723.upstash.io',
+    token: 'AZ8TACQgOTI1ODlhYTUtMjhhOS00NzI2LWIyZWYtNDkwMGNhYTUzMTRmMTM0MTM3YWZmNmEyNGI3NmExYjVhZjU1YzM4YTkyZDk=',
+  })
+  const jobsCompleted = await redis.get('jobs-completed')
+  if (jobsCompleted !== null) {
+    updateCache(redis).catch(console.error)
+    return Number(jobsCompleted)
+  } else {
+    const jobsCompleted = await updateCache(redis)
+    return jobsCompleted
+  }
+}
+
+const updateCache = async (redis: Redis): Promise<number> => {
+  const jobsCompleted = await getFromInflux()
+  await redis.set('jobs-completed', jobsCompleted)
+  return jobsCompleted
+}
+
+export const getFromInflux = async (): Promise<number> => {
   const res = await fetch("https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/query", {
     method: "POST",
     headers: {
--- lib/getJobsCompletedCount_test.ts
+++ lib/getJobsCompletedCount_test.ts
@@ -1,8 +1,27 @@
 import { assert } from "https://deno.land/std@0.162.0/testing/asserts.ts";
-import getJobsCompletedCount from "./getJobsCompletedCount.ts";
+import getJobsCompletedCount, { getFromInflux } from "./getJobsCompletedCount.ts";
 
-Deno.test("getRepositoryDownloadCount", async () => {
-  const jobsCompleted = await getJobsCompletedCount()
-  assert(jobsCompleted > 1_000_000)
+// This test is expected to leak an async op, from updating the cache after
+// sending the response.
+Deno.test({
+  name: "getJobsCompletedCount",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  async fn () {
+    const jobsCompleted = await getJobsCompletedCount()
+    assert(jobsCompleted > 1_000_000)
+  }
+})
+
+// For some reason this test also gets async op warnings. Maybe this is still
+// residue from the previous one.
+Deno.test({
+  name: "getFromInflux",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  async fn () {
+    const jobsCompleted = await getFromInflux()
+    assert(jobsCompleted > 1_000_000)
+  }
 })
 
