6a83f6cb0e80f5d1d9e622313024d8b1c763cd8d
Julian Gruber <julian@juliangruber.com>
2023-05-30 13:05:39 +0200

    add redis cache for slow `jobs-completed`

--- lib/getJobsCompletedCount_test.ts
+++ lib/getJobsCompletedCount_test.ts
@@ -1,8 +1,20 @@
 import { assert } from "https://deno.land/std@0.162.0/testing/asserts.ts";
-import getJobsCompletedCount from "./getJobsCompletedCount.ts";
+import getJobsCompletedCount, { getFromInflux } from "./getJobsCompletedCount.ts";
 
-Deno.test("getRepositoryDownloadCount", async () => {
-  const jobsCompleted = await getJobsCompletedCount()
+// This test is expected to leak an async op, from updating the cache after
+// sending the response.
+Deno.test({
+  name: "getJobsCompletedCount",
+  sanitizeOps: false,
+  sanitizeResources: false,
+  async fn () {
+    const jobsCompleted = await getJobsCompletedCount()
+    assert(jobsCompleted > 1_000_000)
+  }
+})
+
+Deno.test("getFromInflux", async () => {
+  const jobsCompleted = await getFromInflux()
   assert(jobsCompleted > 1_000_000)
 })
 
