d4f9429e8b46d2016021bada7ba2d88498a3f6c3
Julian Gruber <julian@juliangruber.com>
2023-10-31 14:44:38 +0100

    update token

--- downloads.ts
+++ downloads.ts
@@ -8,7 +8,7 @@ app.use(async (ctx: Context) => {
   ctx.response.body = {
     downloads: await getRepositoryDownloadCount(
       "filecoin-station/filecoin-station",
-      "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"
+      "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"
     )
   }
 });
