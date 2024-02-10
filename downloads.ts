79fd32ee344ac8b1fc641a2c6399116cc24f9aa9
Julian Gruber <julian@juliangruber.com>
2024-02-10 13:29:02 +0100

    downloads: remove hardcoded github token

--- downloads.ts
+++ downloads.ts
@@ -1,6 +1,9 @@
 import { Application, Context } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import getRepositoryDownloadCount from "./lib/getRepositoryDownloadCount.ts";
+import { assert } from "https://deno.land/std@0.162.0/testing/asserts.ts";
+
+assert(Deno.env.get("GITHUB_TOKEN"), "$GITHUB_TOKEN required");
 
 const app = new Application();
 app.use(oakCors());
@@ -8,7 +11,7 @@ app.use(async (ctx: Context) => {
   ctx.response.body = {
     downloads: await getRepositoryDownloadCount(
       "filecoin-station/filecoin-station",
-      "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"
+      Deno.env.get("GITHUB_TOKEN")
     )
   }
 });
