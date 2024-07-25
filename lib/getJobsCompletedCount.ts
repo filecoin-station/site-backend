05d9c2c22ed209b4eb1321e304424b2f7564477f
Julian Gruber <julian@juliangruber.com>
2024-07-25 15:37:12 +0200

    add log

--- lib/getJobsCompletedCount.ts
+++ lib/getJobsCompletedCount.ts
@@ -20,11 +20,13 @@ const OFFSET = 155_153_052_475
 const OFFSET_DATE = '2024-07-09'
 
 export const getFromApi = async (): Promise<number> => {
+  console.log('getFromApi() ...')
   const res = await fetch(`https://stats.filspark.com/measurements/daily?from=${OFFSET_DATE}`)
   const days = await res.json()
   const total = days.reduce(
     (acc: number, day: any) => acc + day.accepted_measurement_count,
     0
   )
+  console.log('getFromApi() ✔️')
   return OFFSET + total
 }
